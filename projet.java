c0eeefdcc03b1284dc84c7c4f7fe5ca05d8e6e82
Naillyk21 <Naillyk21@live.fr>
2022-04-25 18:40:41 +0200

    Bug boucle scan

--- projet.java
+++ projet.java
@@ -9,14 +9,27 @@ public class projet
     
     public static void main (String [] args) throws Exception
     {
-        int tab [][]= new int [88234][2];
+        
         //File doc = new File("C:/Users/Dylan/Desktop/Semestre 6/Graphes/projet/testgraphe.txt");
-        File doc = new File("D:/Informatique/projetgraphes/graphes.txt"); 
+        File doc = new File("D:/Informatique/projetgraphes/testgraphe.txt"); 
         Scanner scan = new Scanner(doc);
-        int cpt = 0;
+        int cptligne = 0;
+
+        //Premier scan pour compter le nombre de ligne
         while(scan.hasNextLine())
         {
-            String line = scan.nextLine();
+            cptligne++;
+        }
+        scan.close();
+
+
+        Scanner scan2 = new Scanner(doc);
+        int cpt = 0;
+        int sommetmax = 0;
+        int tab [][]= new int [cptligne][2];
+        while(scan2.hasNextLine())
+        {
+            String line = scan2.nextLine();
             //Motif a chercher dans la chaîne
             Pattern p = Pattern.compile(" ");
             //Recherche dans la chaine
@@ -27,9 +40,31 @@ public class projet
                 //Donne l'endroit ou est trouvé le Pattern p
                 trouve=pos_espace.start();    
             }
+            //Stock dans le tableau
             tab[cpt][0] = Integer.parseInt(line.substring(0,trouve));
             tab[cpt][1] = Integer.parseInt(line.substring(trouve+1,line.length()));
+            if(tab[cpt][1]>sommetmax)
+            {
+                sommetmax =tab[cpt][1];
+            }
+            //Affiche le tableau a chaque ligne d'éxecution
             System.out.println(tab[cpt][0] + "-" + tab[cpt][1]);
+            cpt++;
+        }
+        scan2.close();
+
+        //Tableau de la taille du sommet maximum
+        int degre[][] = new int [sommetmax+1][2];
+        System.out.println("Sommet max : " + sommetmax);
+
+
+
+        //Remplissage du tableau de degré par N°Sommet / 0
+        for(int i = 0;i<=sommetmax;i++)
+        {
+        degre[i][0] = i;
+        degre[i][1] = 0;
+        System.out.println(degre[i][0] + "|" + degre[i][1]);
         }
     }   
 
