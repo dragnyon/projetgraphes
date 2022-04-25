b1c8bd11a2db4ab6233a7f15324d7351c1e27014
Naillyk21 <Naillyk21@live.fr>
2022-04-25 17:49:30 +0200

    Récupère adjacence + stock dans le tableau

--- projet.java
+++ projet.java
@@ -9,10 +9,11 @@ public class projet
     
     public static void main (String [] args) throws Exception
     {
-        int tab [][]= new int [100][100];
+        int tab [][]= new int [88234][2];
         //File doc = new File("C:/Users/Dylan/Desktop/Semestre 6/Graphes/projet/testgraphe.txt");
         File doc = new File("D:/Informatique/projetgraphes/graphes.txt"); 
         Scanner scan = new Scanner(doc);
+        int cpt = 0;
         while(scan.hasNextLine())
         {
             String line = scan.nextLine();
@@ -26,16 +27,10 @@ public class projet
                 //Donne l'endroit ou est trouvé le Pattern p
                 trouve=pos_espace.start();    
             }
-            //Récupère la partie int avant l'espace
-            int partie1int = Integer.parseInt(line.substring(0,trouve));
-            //Récupère la partie int après l'espace
-            int partie2int = Integer.parseInt(line.substring(trouve+1,line.length()));
-            System.out.println(partie1int);
-            System.out.println(partie2int);
+            tab[cpt][0] = Integer.parseInt(line.substring(0,trouve));
+            tab[cpt][1] = Integer.parseInt(line.substring(trouve+1,line.length()));
+            System.out.println(tab[cpt][0] + "-" + tab[cpt][1]);
         }
-        
-        
-     
     }   
 
 }
