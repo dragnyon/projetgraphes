05f1ad20b9b455684853a40f69db99cb2a66beeb
dragnyon <81358334+dragnyon@users.noreply.github.com>
2022-04-25 15:40:13 +0200

    Add files via upload

---
+++ projet.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+
+public class projet
+{
+    
+    public static void main (String [] args) throws Exception
+    {
+        int tab [][]= new int [100][100];
+        File doc = new File("C:/Users/Dylan/Desktop/Semestre 6/Graphes/projet/testgraphe.txt");
+        Scanner scan = new Scanner(doc);
+        while(scan.hasNextLine())
+        {
+
+            String line = scan.nextLine();
+            System.out.println(line);
+            
+        }
+        scan.close();
+     
+    }   
+
+}
