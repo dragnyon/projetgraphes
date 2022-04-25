cf2be3a9205d09db9103a34c843d88bf7e61e166
Naillyk21 <Naillyk21@live.fr>
2022-04-25 16:43:42 +0200

    Ajout de ma ligne scanfile

--- projet.java
+++ projet.java
@@ -8,14 +8,17 @@ public class projet
     public static void main (String [] args) throws Exception
     {
         int tab [][]= new int [100][100];
-        File doc = new File("C:/Users/Dylan/Desktop/Semestre 6/Graphes/projet/testgraphe.txt");
+        //File doc = new File("C:/Users/Dylan/Desktop/Semestre 6/Graphes/projet/testgraphe.txt");
+        File doc = new File("D:/Informatique/projetgraphes/graphes.txt"); 
         Scanner scan = new Scanner(doc);
         while(scan.hasNextLine())
         {
             //lit le fichier
             String line = scan.nextLine();
-            System.out.println(line);
-            
+            String[] decoupage = line.split(" ");
+            for (int i = 0; i < decoupage.length; i++) {
+                System.out.println(decoupage[i]);
+            }
         }
         scan.close();
      
