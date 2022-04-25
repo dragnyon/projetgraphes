6814241fe74b2320270825ad1d5867f7cb3ee8f0
Naillyk21 <Naillyk21@live.fr>
2022-04-25 17:35:34 +0200

    Récup les entiers séparément

--- projet.java
+++ projet.java
@@ -1,6 +1,8 @@
 import java.io.*;
 import java.util.*;
-
+import java.lang.*;
+import java.util.regex.Matcher;  
+import java.util.regex.Pattern;  
 
 public class projet
 {
@@ -13,14 +15,29 @@ public class projet
         Scanner scan = new Scanner(doc);
         while(scan.hasNextLine())
         {
-            //lit le fichier
             String line = scan.nextLine();
-            String[] decoupage = line.split(" ");
-            for (int i = 0; i < decoupage.length; i++) {
-                System.out.println(decoupage[i]);
+            //Motif a chercher dans la chaîne
+            Pattern p = Pattern.compile(" ");
+            //Recherche dans la chaine
+            Matcher pos_espace = p.matcher(line);
+            int trouve=0;
+            if(pos_espace.find())
+            {
+                //Donne l'endroit ou est trouvé le Pattern p
+                trouve=pos_espace.start();    
             }
+            //Decoupe la chaine de 0 a l'espace
+            String partie1 = line.substring(0,trouve);
+            //Decoupe la chaine de l'espace a la fin
+            String partie2 = line.substring(trouve+1,line.length());
+            //Récupère les int de la string
+            int partie1int = Integer.parseInt(partie1);
+            int partie2int = Integer.parseInt(partie2);
+            System.out.println(partie1int);
+            System.out.println(partie2int);
         }
-        scan.close();
+        
+        
      
     }   
 
