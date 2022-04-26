937bff4a2eb6ea508411aee44a1578ff863baea8
Naillyk21 <Naillyk21@live.fr>
2022-04-26 15:02:45 +0200

    Ajoutertrycatch

--- projet.java
+++ projet.java
@@ -10,74 +10,83 @@ public class projet
     public static void main (String [] args) throws Exception
     {
         
-        //File doc = new File("C:/Users/Dylan/Desktop/Semestre 6/Graphes/projet/testgraphe.txt");
-        File doc = new File("D:/Informatique/projetgraphes/graphes.txt"); 
-        Scanner scan = new Scanner(doc);
-        int cptligneadj = 0;
+        
+        String url = "D:/Informatique/projetgraphes/graphes.txt";
+        //String url = "C:/Users/Dylan/Desktop/Semestre 6/Graphes/projet/testgraphe.txt";
 
-        //Premier scan pour compter le nombre de ligne
-        while(scan.hasNextLine())
-        {
-            cptligneadj++;
-            scan.nextLine();
-        }
-        scan.close();
+        getdegre(url);
+    }
 
-        Scanner scan2 = new Scanner(doc);
-        int cpt = 0;
-        int sommetmax = 0;
-        int tab [][]= new int [cptligneadj][2];
-        while(scan2.hasNextLine())
+    static public void getdegre(String url)
         {
-            String line = scan2.nextLine();
-            //Motif a chercher dans la chaîne
-            Pattern p = Pattern.compile(" ");
-            //Recherche dans la chaine
-            Matcher pos_espace = p.matcher(line);
-            int trouve=0;
-            if(pos_espace.find())
+            File doc = new File(url); 
+            Scanner scan = new Scanner(doc);
+            int cptligneadj = 0;
+
+            //Premier scan pour compter le nombre de ligne
+            while(scan.hasNextLine())
             {
-                //Donne l'endroit ou est trouvé le Pattern p
-                trouve=pos_espace.start();    
+                cptligneadj++;
+                scan.nextLine();
             }
-            //Stock dans le tableau
-            tab[cpt][0] = Integer.parseInt(line.substring(0,trouve));
-            tab[cpt][1] = Integer.parseInt(line.substring(trouve+1,line.length()));
-            if(tab[cpt][1]>sommetmax)
+            scan.close();
+
+            Scanner scan2 = new Scanner(doc);
+            int cpt = 0;
+            int sommetmax = 0;
+            int tab [][]= new int [cptligneadj][2];
+            while(scan2.hasNextLine())
             {
-                sommetmax =tab[cpt][1];
+                String line = scan2.nextLine();
+                //Motif a chercher dans la chaîne
+                Pattern p = Pattern.compile(" ");
+                //Recherche dans la chaine
+                Matcher pos_espace = p.matcher(line);
+                int trouve=0;
+                if(pos_espace.find())
+                {
+                    //Donne l'endroit ou est trouvé le Pattern p
+                    trouve=pos_espace.start();    
+                }
+                //Stock dans le tableau
+                tab[cpt][0] = Integer.parseInt(line.substring(0,trouve));
+                tab[cpt][1] = Integer.parseInt(line.substring(trouve+1,line.length()));
+                if(tab[cpt][1]>sommetmax)
+                {
+                    sommetmax =tab[cpt][1];
+                }
+                cpt++;
             }
-            cpt++;
-        }
-        scan2.close();
+            scan2.close();
 
-        //Tableau de la taille du sommet maximum
-        int degre[][] = new int [sommetmax+1][2];
-        System.out.println("Sommet max : " + sommetmax);
+            //Tableau de la taille du sommet maximum
+            int degre[][] = new int [sommetmax+1][2];
+            System.out.println("Sommet max : " + sommetmax);
 
 
 
-        //Remplissage du tableau de degré par N°Sommet / 0
-        for(int i = 0;i<=sommetmax;i++)
-        {
-        degre[i][0] = i;
-        //degre[i][1] = 0;
-        }
-
-        //remplissage des degré de chaque sommet
-        for(int i = 0; i<cptligneadj;i++)
-        {
-            for(int j = 0 ; j<2 ; j++)
+            //Remplissage du tableau de degré par N°Sommet / 0
+            for(int i = 0;i<=sommetmax;i++)
             {
-                degre[tab[i][j]][1]++;
+            degre[i][0] = i;
+            //degre[i][1] = 0;
+            }
+
+            //remplissage des degré de chaque sommet
+            for(int i = 0; i<cptligneadj;i++)
+            {
+                for(int j = 0 ; j<2 ; j++)
+                {
+                    degre[tab[i][j]][1]++;
+                }
+            }
+
+            //Affichage du tableau des degrés
+            for(int i=0;i<sommetmax;i++)
+            {
+                System.out.println("Sommet : "+degre[i][0]+" Degré : "+degre[i][1]);
             }
         }
 
-        //Affichage du tableau des degrés
-        for(int i=0;i<sommetmax;i++)
-        {
-            System.out.println("Sommet : "+degre[i][0]+" Degré : "+degre[i][1]);
-        }
-    }   
 
 }
